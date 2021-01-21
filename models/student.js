5ec5bb2ebab6a184effff1655556c3dee0aafe90
Zac Edens <themancalledzac@gmail.com>
2021-01-20 17:01:14 -0800

    initial student model file

---
+++ models/student.js
@@ -0,0 +1,44 @@
+module.exports = function (sequelize, DataTypes) {
+    const Students = sequelize.define("Students", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 50]
+            }
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 50]
+            }
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 25]
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            validate: {
+                isEmail: true,
+            }
+        }
+    });
+
+    Students.associate = models => {
+        Students.belongsTo(models.TeacherStudents, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    }
+
+    return Students;
+};
