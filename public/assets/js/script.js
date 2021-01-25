b17be03cb007dc434a1613bdddca73266e050d13
ademir <60299587+alidemirbay@users.noreply.github.com>
2021-01-24 22:40:47 -0800

    added routes for class page and function to script js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -130,3 +130,22 @@ $(document).ready(() => {
     $(".email").text(data.email);
   });
 });
+
+/* -------------------------------------------------------------------------- */
+/*                                    class                                   */
+/* -------------------------------------------------------------------------- */
+
+
+
+
+function getClasses() {
+  $.get("/api/class").then(data => {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      const liEL = $("<li>");
+      liEL.addClass("list-group-item").text(data[i].name);
+      $(".list-group-class").append(liEL)
+    }
+  });
+}
+getClasses();
